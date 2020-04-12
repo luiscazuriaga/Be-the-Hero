a39a844916c80c223c8fbb5917f7c581c19748cc
luis cazuriaga <51897512+luis-cazuriaga@users.noreply.github.com>
2020-04-12 18:40:45 -0300

    Update routes.js

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -17,32 +17,3 @@ export default function Routes(){
 );
 }
 
-
-
-// import React from 'react';
-// import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
-
-// const { Navigator, Screen  } = createStackNavigator();
-
-// import Incidents from './pages/Incidents';
-// import Detail from './pages/Detail';
-
-// export default function routes() {
-//   return (
-//     <Navigator
-//       initialRouteName="Incidents"
-//       headerMode="none"    
-//     >
-//       <Screen name="Incidents" component={Incidents} />
-//       <Screen 
-//         name="Detail"
-//         component={Detail}
-//         options={{
-//           gestureEnabled: true,
-//           cardOverlayEnabled: true,                    
-//           ...TransitionPresets.SlideFromRightIOS,
-//         }}
-//       />
-//     </Navigator>
-//   );
-// }
